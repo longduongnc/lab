a8f95ad38bcd93d23cc6cc13fdb5f6ea978a5941
Strifhapligity <61440398+longduongnc@users.noreply.github.com>
2021-11-10 19:58:22 -0500

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,20 @@
+public class Deck {
+    public Card drawCard(){
+        
+        int value = (int)(Math.random()*12)+1;
+        int cardSuit = (int)(Math.random()*3)+1;
+        String suit;
+
+        if (cardSuit ==1){
+            suit = "Spades";
+        } else if (cardSuit == 2){
+            suit = "Clubs";
+        } else if (cardSuit == 3){
+            suit = "Hearts";
+        } else {
+            suit = "Diamonds";
+        }
+
+        return new Card(value, suit);
+    }
+}
---
+++ HighLowGame.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class HighLowGame {
+    public static void main(String[] args) throws InterruptedException {
+        
+        Deck deck = new Deck();
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("enter 1 to play 0 to exit");
+        int play = scnr.nextInt();
+        
+        while (play == 1){
+            Card card1 = deck.drawCard();
+            Thread.sleep(500);
+            System.out.println("The first card is " + card1.declareCard());
+            Thread.sleep(500);
+            System.out.println("Will the next card higher or lower? ");
+            Thread.sleep(500);
+            System.out.println("Enter 1 for lower, 2 for higher.");
+
+            int choice = scnr.nextInt();
+
+            Card card2 = deck.drawCard();
+            boolean higher = card2.getValue(false) > card1.getValue(false);
+            if (card1.getValue(false) == card2.getValue(false)){
+                System.out.println("Card Value were the same, no winner or loser this round.");
+            } else if ((higher && choice ==2)||(!higher && choice ==1)){
+                System.out.println("Winner!");
+            } else {
+                System.out.println("Sorry, your guess was incorrect :(");
+            }
+
+            Thread.sleep(500);
+            System.out.println("enter 1 to play again, 0 to exit");
+            play = scnr.nextInt();
+        }
+
+        scnr.close();
+    }
+}
